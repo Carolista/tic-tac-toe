fb3f5297479aa8b65d9b55fa11ff466d5b6dec0d
Carolista <cjones.renaissancewoman@gmail.com>
2025-05-17 21:51:50 -0400

    added some TODOs before branching off

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Palettes from './components/Palettes';
 // TODO: handle a no-win scenario
 // TODO: add a settings modal
 // TODO: add dark mode and light mode
+// TODO: let player choose a color palette
+// TODO: make some palettes unavailable and 
+// unlock them at intervals as user plays more games
 
 function App() {
 	const [currentPlayer, setCurrentPlayer ] = useState(["X", "O"][Math.floor(Math.random() * 2)]);
@@ -18,7 +21,7 @@ function App() {
 			<Header />
 			<Main currentPlayer={currentPlayer} setCurrentPlayer={setCurrentPlayer} />
 			<Footer player={currentPlayer} />
-            <Palettes />
+            {/* <Palettes /> TEMP for testing */}
 		</div>
 	);
 }
