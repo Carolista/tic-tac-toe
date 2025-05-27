984e795111f3e191c3aba0ade4f48e463d79008d
Carolista <cjones.renaissancewoman@gmail.com>
2025-05-27 02:57:11 -0400

    improved routing and added redirect to /play

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,58 @@
 import './App.css';
 import Home from './components/Home.jsx';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
+import { useEffect, useState } from 'react';
+import DarkModeContext from './contexts/DarkModeContext.js';
+import CurrentPlayerContext from './contexts/CurrentPlayerContext.js';
+import CurrentPaletteContext from './contexts/CurrentPaletteContext';
+import { getRandomElement } from './common/utils.js';
+import { palettes } from './common/colors.js';
+import About from './components/about/About.jsx';
 
 // POSSIBLE FUTURE UPDATES
-// TODO: make some palettes unavailable and
-// unlock them at intervals as user plays more games
+// TODO: Make some palettes unavailable and unlock them at intervals as user plays more games
+// TODO: Add rotating images in About component to demonstrate win patterns
 
 function App() {
+	let initialPalette =
+		JSON.parse(localStorage.getItem('palette')) || getRandomElement(palettes);
+	let initialMode = JSON.parse(localStorage.getItem('darkMode')) || false;
+
+	const [currentPlayer, setCurrentPlayer] = useState(
+		['X', 'O'][Math.floor(Math.random() * 2)]
+	);
+	const [currentPalette, setCurrentPalette] = useState(initialPalette);
+	const [darkMode, setDarkMode] = useState(initialMode);
+
+	// Handle topmost level of DOM so that viewport also matches
+	useEffect(() => {
+		document
+			.querySelector('html')
+			.classList.add(darkMode ? 'dark-mode' : 'light-mode');
+		document
+			.querySelector('html')
+			.classList.remove(darkMode ? 'light-mode' : 'dark-mode');
+	}, [darkMode]);
+
 	return (
-        <Home />
-    )
+		<div id="window" className={darkMode ? 'dark-mode' : 'light-mode'}>
+			<CurrentPaletteContext.Provider
+				value={{ currentPalette, setCurrentPalette }}>
+				<CurrentPlayerContext.Provider
+					value={{ currentPlayer, setCurrentPlayer }}>
+					<DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
+						<BrowserRouter>
+							<Routes>
+								<Route path="/" element={<Navigate to="/play" replace />} />
+								<Route path="/play" element={<Home />} />
+								<Route path="/about" element={<About />} />
+							</Routes>
+						</BrowserRouter>
+					</DarkModeContext.Provider>
+				</CurrentPlayerContext.Provider>
+			</CurrentPaletteContext.Provider>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import DarkModeContext from '../contexts/DarkModeContext';
+import CurrentPaletteContext from '../contexts/CurrentPaletteContext';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import SettingsModal from './settings/SettingsModal';
-import DarkModeContext from '../contexts/DarkModeContext';
-import CurrentPlayerContext from '../contexts/CurrentPlayerContext.js';
-import CurrentPaletteContext from '../contexts/CurrentPaletteContext';
-import { getRandomElement } from '../common/utils.js';
-import { palettes } from '../common/colors.js';
 import StatsModal from './stats/StatsModal.jsx';
-import About from './about/About.jsx';
 
 function Home() {
-	let initialPalette =
-		JSON.parse(localStorage.getItem('palette')) || getRandomElement(palettes);
-	let initialMode = JSON.parse(localStorage.getItem('darkMode')) || false;
+	const { currentPalette } = useContext(CurrentPaletteContext);
+	const { darkMode } = useContext(DarkModeContext);
+
 	let initialStats = JSON.parse(localStorage.getItem('stats')) || {
 		gamesPlayed: 0,
 		gamesWonByX: 0,
 		gamesWonByO: 0,
 	};
 
-	const [currentPlayer, setCurrentPlayer] = useState(
-		['X', 'O'][Math.floor(Math.random() * 2)]
-	);
 	const [showSettingsModal, setShowSettingsModal] = useState(false);
 	const [showStatsModal, setShowStatsModal] = useState(false);
-	const [currentPalette, setCurrentPalette] = useState(initialPalette);
-	const [darkMode, setDarkMode] = useState(initialMode);
 	const [stats, setStats] = useState(initialStats);
-
-	// Handle topmost level of DOM so that viewport also matches
-	useEffect(() => {
-		document
-			.querySelector('html')
-			.classList.add(darkMode ? 'dark-mode' : 'light-mode');
-		document
-			.querySelector('html')
-			.classList.remove(darkMode ? 'light-mode' : 'dark-mode');
-	}, [darkMode]);
 
 	useEffect(() => {
 		localStorage.setItem('palette', JSON.stringify(currentPalette));
@@ -61,46 +41,24 @@ function Home() {
 	const handleCloseStatsModal = () => setShowStatsModal(false);
 
 	return (
-		<div id="window" className={darkMode ? 'dark-mode' : 'light-mode'}>
-			<CurrentPaletteContext.Provider
-				value={{ currentPalette, setCurrentPalette }}>
-				<CurrentPlayerContext.Provider
-					value={{ currentPlayer, setCurrentPlayer }}>
-					<DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
-						<BrowserRouter>
-							<Routes>
-								<Route
-									path="/"
-									element={
-										<>
-											<Header />
-											<Main statsPair={{ stats, setStats }} />
-											{showSettingsModal &&
-												createPortal(
-													<SettingsModal
-														closeModal={handleCloseSettingsModal}
-													/>,
-													document.getElementById('modal-root')
-												)}
-											{showStatsModal &&
-												createPortal(
-													<StatsModal closeModal={handleCloseStatsModal} />,
-													document.getElementById('modal-root')
-												)}
-											<Footer
-												openSettingsModal={handleOpenSettingsModal}
-												openStatsModal={handleOpenStatsModal}
-											/>
-										</>
-									}
-								/>
-								<Route path="/about" element={<About />} />
-							</Routes>
-						</BrowserRouter>
-					</DarkModeContext.Provider>
-				</CurrentPlayerContext.Provider>
-			</CurrentPaletteContext.Provider>
-		</div>
+		<>
+			<Header />
+			<Main statsPair={{ stats, setStats }} />
+			<Footer
+				openSettingsModal={handleOpenSettingsModal}
+				openStatsModal={handleOpenStatsModal}
+			/>
+			{showSettingsModal &&
+				createPortal(
+					<SettingsModal closeModal={handleCloseSettingsModal} />,
+					document.getElementById('modal-root')
+				)}
+			{showStatsModal &&
+				createPortal(
+					<StatsModal closeModal={handleCloseStatsModal} />,
+					document.getElementById('modal-root')
+				)}
+		</>
 	);
 }
 
